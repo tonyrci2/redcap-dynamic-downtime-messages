31a7d5d599b2a6787c877f8bbb72f040744d6c84
tonyrci2 <79281146+tonyrci2@users.noreply.github.com>
2021-02-18 20:07:55 -0500

    Fix identing

--- DynamicDowntimeMessages.php
+++ DynamicDowntimeMessages.php
@@ -40,68 +40,68 @@ class DynamicDowntimeMessages extends AbstractExternalModule {
 		// print_r(get_defined_constants());
 		if (PAGE === 'index.php' || PAGE === 'redcap/index.php' || PAGE === 'index.php?action=myprojects' || PAGE === 'redcap/index.php?action=myprojects') {
 		$timezone=strip_tags($this->getSystemSetting('timezone'));
-		// *Always* use a timezone identifier like "Europe/London" and not an abbreviation such as "GMT", since the latter will not account for daylight savings time, whereas the former will.
-		// https://stackoverflow.com/questions/17694894/different-timezone-types-on-datetime-object
-		// What's nice about using the tz identifier is that, when printing a date/time, depending on the date, the correct timezone abbreviation (EST vs EDT) is automatically outputted.
-		\Moment\Moment::setLocale('en_US');
-		\Moment\Moment::setDefaultTimezone($timezone);
-		$startTime=$this->getSystemSetting('datetime-down');
-		$downTimeMinutes=(int) $this->getSystemSetting('duration-down');
-		$m = new \Moment\Moment($startTime,$timezone);
-		// echo $m->format() . '<br>';
-		$e = $m->cloning()->addMinutes($downTimeMinutes);
+			// *Always* use a timezone identifier like "Europe/London" and not an abbreviation such as "GMT", since the latter will not account for daylight savings time, whereas the former will.
+			// https://stackoverflow.com/questions/17694894/different-timezone-types-on-datetime-object
+			// What's nice about using the tz identifier is that, when printing a date/time, depending on the date, the correct timezone abbreviation (EST vs EDT) is automatically outputted.
+			\Moment\Moment::setLocale('en_US');
+			\Moment\Moment::setDefaultTimezone($timezone);
+			$startTime=$this->getSystemSetting('datetime-down');
+			$downTimeMinutes=(int) $this->getSystemSetting('duration-down');
+			$m = new \Moment\Moment($startTime,$timezone);
+			// echo $m->format() . '<br>';
+			$e = $m->cloning()->addMinutes($downTimeMinutes);
 
-		if ($downTimeMinutes < 60 && $downTimeMinutes > 0) {
-			$downTimeHuman = round($downTimeMinutes,0) . "&nbsp;minutes.";
-		}
-		elseif (round($downTimeMinutes/60,2,PHP_ROUND_HALF_DOWN) == 1.0) {
-			$downTimeHuman = 'one&nbsp;hour';
-		}
-		else {
-			$downTimeHuman = round($downTimeMinutes/60/.25,0,PHP_ROUND_HALF_DOWN)*.25 . "&nbsp;hours";
-		}
+			if ($downTimeMinutes < 60 && $downTimeMinutes > 0) {
+				$downTimeHuman = round($downTimeMinutes,0) . "&nbsp;minutes.";
+			}
+			elseif (round($downTimeMinutes/60,2,PHP_ROUND_HALF_DOWN) == 1.0) {
+				$downTimeHuman = 'one&nbsp;hour';
+			}
+			else {
+				$downTimeHuman = round($downTimeMinutes/60/.25,0,PHP_ROUND_HALF_DOWN)*.25 . "&nbsp;hours";
+			}
 
 
-		// echo $e->format(); 
-		// echo '<br>';
+			// echo $e->format(); 
+			// echo '<br>';
 
-		// echo $e->getHour() . '<br>';
-		// echo $e->getMinute() . '<br>';
-		// echo $e->format('g:i A') . '<br>';
+			// echo $e->getHour() . '<br>';
+			// echo $e->getMinute() . '<br>';
+			// echo $e->format('g:i A') . '<br>';
 
-		$fullDate = $m->format('l, F jS, Y');
-		$startTime = $m->format('g:i');
-		$endTime = $e->format('g:i A T');
-		// echo $fullDate . '<br>';
-		// echo $startTime . '<br>';
-		// echo $endTime. '<br>';
+			$fullDate = $m->format('l, F jS, Y');
+			$startTime = $m->format('g:i');
+			$endTime = $e->format('g:i A T');
+			// echo $fullDate . '<br>';
+			// echo $startTime . '<br>';
+			// echo $endTime. '<br>';
 
-		// echo '<br>';
-		$momentFromVo = $m->fromNow();
+			// echo '<br>';
+			$momentFromVo = $m->fromNow();
 
-		$relative = $momentFromVo->getRelative();
-		// echo $relative;
-		// echo "<br>";
+			$relative = $momentFromVo->getRelative();
+			// echo $relative;
+			// echo "<br>";
 
-		$relCal =  $m->calendar();
-		// echo $relCal;
-		// echo "<br>";
+			$relCal =  $m->calendar();
+			// echo $relCal;
+			// echo "<br>";
 
-		// Will only display if downtime comes after the present time.
+			// Will only display if downtime comes after the present time.
 
-		if ($momentFromVo->getHours() < 0) {
-			$homepageNotice= '<div class="' . $this->getSystemSetting('div-class') . '" style="text-align:center">'.'<u><b><span style="font-size:120%">Notice of Planned Outage</span></b></u><br><br>Due to ' . $this->getSystemSetting('reason-down') . ', REDCap' . $institutionText . ' will be down briefly, &nbsp;<span style="font-weight:bold;font-size:110%;">'."*{$relative}*</span>,&nbsp;on<br><br><h6><b>{$fullDate}</b>,</h6>".'<span style="font-weight:bold;font-size:115%">'." {$startTime} - {$endTime}.</span>".'<p style="text-align:center;padding-top:6px"></b>Thank you for your patience as we work to keep REDCap up to date and running smoothly.</p></div>';
-			?>
-            <script type='text/javascript'>
-                $(document).ready(function() {
-                	if ($("div[style|='margin-bottom:10px;']").length >= 1) {$("div[style|='margin-bottom:10px;']")[0].innerHTML='<?php echo $homepageNotice; ?>'}
- 					else {$("nav[role|='navigation']").after('<div style="margin-bottom:10px;"><?php echo $homepageNotice; ?></div>')}; 
-                });
-                //*[@id="pagecontent"]/div[1]/div
-            </script>
-            <?php
+			if ($momentFromVo->getHours() < 0) {
+				$homepageNotice= '<div class="' . $this->getSystemSetting('div-class') . '" style="text-align:center">'.'<u><b><span style="font-size:120%">Notice of Planned Outage</span></b></u><br><br>Due to ' . $this->getSystemSetting('reason-down') . ', REDCap' . $institutionText . ' will be down briefly, &nbsp;<span style="font-weight:bold;font-size:110%;">'."*{$relative}*</span>,&nbsp;on<br><br><h6><b>{$fullDate}</b>,</h6>".'<span style="font-weight:bold;font-size:115%">'." {$startTime} - {$endTime}.</span>".'<p style="text-align:center;padding-top:6px"></b>Thank you for your patience as we work to keep REDCap up to date and running smoothly.</p></div>';
+				?>
+		    <script type='text/javascript'>
+			$(document).ready(function() {
+				if ($("div[style|='margin-bottom:10px;']").length >= 1) {$("div[style|='margin-bottom:10px;']")[0].innerHTML='<?php echo $homepageNotice; ?>'}
+						else {$("nav[role|='navigation']").after('<div style="margin-bottom:10px;"><?php echo $homepageNotice; ?></div>')}; 
+			});
+			//*[@id="pagecontent"]/div[1]/div
+		    </script>
+		    <?php
 
-		}
+			}
 		}
 
 
